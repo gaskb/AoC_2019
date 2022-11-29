18723841276291e448d6dda68aa83dab953164f0
Matteo Gasparri <gasparri@dudat.it>
2022-11-29 10:13:16 +0100

    Init AOC-2019 project

---
+++ src/template.js
@@ -0,0 +1,16 @@
+import Utils from './utils';
+
+console.log('Utils');
+console.log(Utils.toString());
+
+class AOC {
+  part1 = async input => {
+    console.log('still nothing to do');
+  };
+
+  part2 = async input => {
+    console.log('still nothing to do');
+  };
+}
+
+export default AOC;
